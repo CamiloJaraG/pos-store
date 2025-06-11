35d6cfdc13251aaf7f0ca140cf98f0b293757cc5
Camilo <camilo.jara5@virginiogomez.cl>
2025-06-11 17:01:42 -0400

    Eliminar configuración de base de datos para desarrollo local en settings.py

--- config/settings.py
+++ config/settings.py
@@ -99,15 +99,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# --- DESARROLLO LOCAL ---
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / "db.sqlite3",
-    }
-}
 # --- PRODUCCIÓN ---
-"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -121,7 +113,6 @@ DATABASES = {
         }
     }
 }
-"""
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
